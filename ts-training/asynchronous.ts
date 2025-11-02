df0513f9b15403fdc1ee301adec8d123a1f1ef4c
Palm <241it002i@s.internet.ac.jp>
2025-11-02 20:42:56 +0900

    非同期処理

---
+++ ts-training/asynchronous.ts
@@ -0,0 +1,13 @@
+import fs, { read } from "fs";
+import util from "util";
+
+const promisifyReadFile = util.promisify(fs.readFile);
+
+async function main() {
+  const data = await promisifyReadFile("package.json");
+
+  const fileContent = data.toString();
+  console.log(fileContent);
+}
+
+main();
---
+++ ts-training/callback.ts
@@ -0,0 +1,31 @@
+function add(v1: number, v2: number) {
+  return v1 + v2;
+}
+
+function calculate(
+  v1: number,
+  v2: number,
+  callback: (a: number, b: number) => number
+): number {
+  return callback(v1, v2);
+}
+
+const addResult = calculate(1, 2, add);
+console.log(addResult);
+
+function multiply(v1: number, v2: number): number {
+  return v1 + v2;
+}
+
+const multiplyResult = calculate(1, 2, multiply);
+console.log(multiplyResult);
+
+const hello = () => {
+  console.log("hello");
+};
+
+setTimeout(hello, 5000);
+
+setTimeout(() => {
+  console.log("hello");
+}, 5000);
