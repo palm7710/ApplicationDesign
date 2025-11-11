25029f9e52ca44050546ff644a7a804f540bdd1c
Palm <241it002i@s.internet.ac.jp>
2025-11-11 21:06:39 +0900

    MYSQLに対戦を保存

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import "express-async-errors";
+import mysql from "mysql2/promise";
+import { createConnection } from "net";
 
 const PORT = 3000;
 
@@ -22,7 +24,24 @@ app.get("/api/error", async (req, res) => {
 
 app.post("/api/games", async (req, res) => {
   const startedAt = new Date();
-  console.log(`startedAt = ${startedAt}`);
+
+  const conn = await mysql.createConnection({
+    host: "localhost",
+    database: "reversi",
+    user: "reversi",
+    password: "password",
+  });
+  try {
+    await conn.beginTransaction();
+
+    await conn.execute("insert into games (started_at) values (?)", [
+      startedAt,
+    ]);
+
+    await conn.commit();
+  } finally {
+    await conn.end();
+  }
 
   res.status(201).end();
 });
