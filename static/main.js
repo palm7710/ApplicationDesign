eb27f26f04af8242ab6b1b81ee62621714a0f7a3
Palm <241it002i@s.internet.ac.jp>
2025-11-13 22:30:05 +0900

    石を打つ処理のフロントエンドの実装

--- static/main.js
+++ static/main.js
@@ -20,13 +20,14 @@ async function showBoard() {
   const response = await fetch("/api/games/latest/turns/${turnCount");
   const responseBody = await response.json();
   const board = responseBody.board;
+  const nextDisc = responseBody.nextDisc;
 
   while (boardElement.firstChild) {
     boardElement.removeChild(boardElement.firstChild);
   }
 
-  board.forEach((line) => {
-    line.forEach((square) => {
+  board.forEach((line, y) => {
+    line.forEach((square, x) => {
       // <div class="square">
       const squareElement = document.createElement("div");
       squareElement.className = "square";
@@ -38,6 +39,11 @@ async function showBoard() {
         stoneElement.className = `stone ${color}`;
 
         squareElement.appendChild(stoneElement);
+      } else {
+        squareElement.addEventListener("click", async () => {
+          const nextTurnCount = turnCount + 1;
+          await registerTurn(nextTurnCount, nextDisc, x, y);
+        });
       }
 
       boardElement.appendChild(squareElement);
@@ -48,6 +54,25 @@ async function showBoard() {
 async function registerGame() {
   await fetch("/api/games", {
     method: "POST",
+  });
+}
+
+async function registerTurn(turnCount, disc, x, y) {
+  const requestBody = {
+    turnCount,
+    move: {
+      disc,
+      x,
+      y,
+    },
+  };
+
+  await fetch("/api/games/latest/turns", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
   });
 }
 
