e39a3a109bf5b801ff138f46f01ccade7a5d6c35
Palm <241it002i@s.internet.ac.jp>
2025-11-14 21:23:53 +0900

    フロントエンドの盤面を取得する処理の修正

--- static/main.js
+++ static/main.js
@@ -15,9 +15,8 @@ const INITIAL_BOARD = [
 
 const boardElement = document.getElementById("board");
 
-async function showBoard() {
-  const turnCount = 0;
-  const response = await fetch("/api/games/latest/turns/${turnCount");
+async function showBoard(turnCount) {
+  const response = await fetch(`/api/games/latest/turns/${turnCount}`);
   const responseBody = await response.json();
   const board = responseBody.board;
   const nextDisc = responseBody.nextDisc;
@@ -43,6 +42,7 @@ async function showBoard() {
         squareElement.addEventListener("click", async () => {
           const nextTurnCount = turnCount + 1;
           await registerTurn(nextTurnCount, nextDisc, x, y);
+          await showBoard(nextTurnCount);
         });
       }
 
@@ -78,7 +78,7 @@ async function registerTurn(turnCount, disc, x, y) {
 
 async function main() {
   await registerGame();
-  await showBoard();
+  await showBoard(0);
 }
 
 main();
